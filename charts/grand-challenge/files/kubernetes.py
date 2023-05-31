41c3eda67c847c3463ea31d11337ea800a797bfb
Pontus Freyhult <pontus_github@soua.net>
2023-05-31 20:11:53 +0200

    Fixes after testing

--- charts/grand-challenge/files/kubernetes.py
+++ charts/grand-challenge/files/kubernetes.py
@@ -1,9 +1,13 @@
 import logging
 from ipaddress import ip_address
-from json import JSONDecodeError
+import json
 from pathlib import Path
 from socket import getaddrinfo
 from tempfile import TemporaryDirectory
+
+import os
+import boto3
+import datetime
 
 import requests
 from dateutil.parser import isoparse
@@ -23,80 +27,125 @@ logger = logging.getLogger(__name__)
 
 
 class KubernetesExecutor(Executor):
+    __s3_client = None
+
+    @staticmethod
+    def _s3_endpoint():
+        endpoint = os.environ.get("AWS_S3_ENDPOINT_URL")
+        if os.environ.get("AWS_S3_INTERNAL_URL"):
+            endpoint = os.environ.get("AWS_S3_INTERNAL_URL")
+
+        return endpoint
+
+    @property
+    def _s3_client(self):
+        if not self.__s3_client:
+            endpoint = self._s3_endpoint()
+            self.__s3_client = boto3.client("s3", endpoint_url=endpoint)
+        return self.__s3_client
+
     @staticmethod
     def serviceaccount_base():
         return "/var/run/secrets/kubernetes.io/serviceaccount/"
 
-    @staticmethod
-    def namespace():
-        with open(serviceaccount_base() + "/namespace") as f:
+    @classmethod
+    def namespace(cls):
+        with open(cls.serviceaccount_base() + "/namespace") as f:
             return f.readline()
 
-    @staticmethod
-    def token():
-        with open(serviceaccount_base() + "/token") as f:
+    @classmethod
+    def token(cls):
+        with open(cls.serviceaccount_base() + "/token") as f:
             return f.readline()
 
     @staticmethod
     def kubeserver_url():
-        return f"https://{os.environ['KUBERNETES_SERVICE_HOST']}:{os.environ['KUBERNETES_SERVICE_PORT_HTTPS']}/"
+        return f"https://{os.environ['KUBERNETES_SERVICE_HOST']}:{os.environ['KUBERNETES_SERVICE_PORT_HTTPS']}"
 
-    @staticmethod
-    def cafile():
-        return serviceaccount_base() + "/ca.crt"
+    @classmethod
+    def cafile(cls):
+        return cls.serviceaccount_base() + "/ca.crt"
 
-    @staticmethod
-    def kubecall(urlpart, method="GET", data=None):
-        baseurl = kubeserver_url()
-        ca = cafile()
-        ns = namespace()
-        h = {"Authorization", f"Bearer {token()}"}
+    @classmethod
+    def kubecall(cls, urlpart, method="GET", data=None):
+        baseurl = cls.kubeserver_url()
+        ca = cls.cafile()
+        ns = cls.namespace()
+        h = {"Authorization": f"Bearer {cls.token()}", "Accept": "application/json"}
+        useurl = f"{baseurl}/{urlpart.replace('_:NS:_', ns).strip('/')}"
 
         kwa = {}
         if data:
+            h["Content-Type"] = "application/json"
             kwa["data"] = data
+
+        logger.debug(f"Calling {useurl} with {method}, passing {h}")
+        logger.debug(f"Keyword arguments {kwa}")
 
         r = requests.request(
             method=method,
-            url=f"{baseurl}/{urlpart.replace('_:NS:_', ns)}",
+            url=useurl,
             verify=ca,
             headers=h,
             **kwa,
         )
 
         if not r.ok:
+            logger.error(f"Call to {useurl} failed, returned {r.text}")
             raise requests.exceptions.RequestException(
                 f"Request response was not ok: got {r.status_code}"
             )
 
         return r
 
-    @staticmethod
-    def get_jobs():
-        return kubecall("/batch/apis/v1/namespaces/_:NS:_/jobs").json()["items"]
+    @classmethod
+    def get_jobs(cls):
+        return cls.kubecall("/apis/batch/v1/namespaces/_:NS:_/jobs").json()["items"]
 
-    @staticmethod
-    def get_pods():
-        return kubecall("/api/v1/namespaces/_:NS:_/pods").json()["items"]
+    @classmethod
+    def get_pods(cls):
+        return cls.kubecall("/api/v1/namespaces/_:NS:_/pods").json()["items"]
 
-    def create_job(self, jobspec):
-        r = kubecall(
-            "/batch/apis/v1/namespaces/_:NS:_/jobs/{self.container_name}", method="POST"
+    @classmethod
+    def get_registry_ip(cls):
+        return cls.kubecall("/api/v1/namespaces/_:NS:_/services/registry").json()[
+            "spec"
+        ]["clusterIP"]
+
+    @classmethod
+    def create_job(cls, jobspec):
+        r = cls.kubecall(
+            f"/apis/batch/v1/namespaces/_:NS:_/jobs/", method="POST", data=jobspec
         )
 
-    def delete_job(self, name):
-        r = kubecall(
-            "/batch/apis/v1/namespaces/_:NS:_/jobs/{self.container_name}",
+    @classmethod
+    def delete_job(cls, name):
+        r = cls.kubecall(
+            f"/apis/batch/v1/namespaces/_:NS:_/jobs/{name}",
             method="DELETE",
         )
 
-    def get_logs(self, name, tailLines=-1):
-        return kubecall(
-            "/api/v1/namespaces/_:NS:_/pods/{name}/log?timestamps{'&'+str(tailLines) if tailLines >0 else ''}"
+    @classmethod
+    def kill_pod(cls, job_name):
+        pod = cls.get_job_pod(job_name)
+
+        if not pod or "metadata" not in pod:
+            return
+
+        name = pod["metadata"]["name"]
+        r = cls.kubecall(
+            f"/api/v1/namespaces/_:NS:_/pods/{name}",
+            method="DELETE",
+        )
+
+    @classmethod
+    def get_logs(cls, name, tailLines=-1):
+        return cls.kubecall(
+            f"/api/v1/namespaces/_:NS:_/pods/{name}/log?timestamps{'&'+str(tailLines) if tailLines >0 else ''}"
         ).text
 
     @property
-    def container_name(self):
+    def job_name(self):
         return self._job_id
 
     def execute(self, *, input_civs, input_prefixes):
@@ -107,7 +156,12 @@ class KubernetesExecutor(Executor):
 
     def deprovision(self):
         super().deprovision()
-        delete_job(name=self.container_name)
+
+        jobs = self.get_jobs()
+        for j in jobs:
+            if "metadata" in j and j["metadata"]["name"] == self.job_name:
+                self.delete_job(name=self.job_name)
+        self.kill_pod(self.job_name)
 
     @staticmethod
     def get_job_params(*, event):
@@ -116,16 +170,43 @@ class KubernetesExecutor(Executor):
     @property
     def duration(self):
         try:
-            details = get_jobs()
+            details = self.get_job_pod(self.job_name)
 
-            for p in details:
-                if p["metadata"]["name"] == self.container_name:
-                    s = p["status"]
+            if not details:
+                return
 
-                    if s["succeeded"] > 0:
-                        started_at = s["startTime"]
-                        finished_at = s["completionTime"]
-                        return isoparse(finished_at) - isoparse(started_at)
+            if "metadata" in details and "creationTimestamp" in details["metadata"]:
+                md = details["metadata"]
+                logger.debug(f"Pod details: {details}")
+
+                started_at = isoparse(md["creationTimestamp"]).timestamp()
+
+                finished_at = datetime.datetime.utcnow().timestamp()
+
+                if "deletionTimestamp" in md:
+                    finished_at = isoparse(md["deletionTimestamp"]).timestamp()
+
+                return datetime.timedelta(seconds=finished_at - started_at)
+
+        except requests.exceptions.RequestException:
+            return None
+        return None
+
+    @classmethod
+    def get_job_pod(cls, job_name):
+        try:
+            details = cls.get_pods()
+
+            for pod in details:
+                if not "metadata" in pod:
+                    continue
+                if not "labels" in pod["metadata"]:
+                    continue
+                if not "job-name" in pod["metadata"]["labels"]:
+                    continue
+
+                if pod["metadata"]["labels"]["job-name"] == job_name:
+                    return pod
 
         except requests.exceptions.RequestException:
             return None
@@ -133,36 +214,16 @@ class KubernetesExecutor(Executor):
 
     @property
     def container_ip(self):
-        try:
-            details = get_pods()
+        pod = self.get_job_pod(self.job_name)
 
-            for pod in details:
-                if (
-                    pod["metadata"]["ownerReferences"][0]["name"] == self.container_name
-                    and pod["metadata"]["ownerReferences"][0]["kind"] == "Job"
-                ):
-                    if "podIP" in pod["status"]:
-                        return pod["status"]["podIP"]
+        if pod and "podIP" in pod["status"]:
+            return pod["status"]["podIP"]
 
-        except requests.exceptions.RequestException:
-            return None
         return None
 
     @property
     def container_logs(self):
-        try:
-            details = get_pods()
-
-            for pod in details:
-                if (
-                    pod["metadata"]["ownerReferences"][0]["name"] == self.container_name
-                    and pod["metadata"]["ownerReferences"][0]["kind"] == "Job"
-                ):
-                    return get_logs(pod["metadata"]["name"])
-
-        except requests.exceptions.RequestException:
-            return None
-        return None
+        return self.get_logs(self.get_job_pod(self.job_name)["metadata"]["name"])
 
     @property
     def cents_per_hour(self):
@@ -194,7 +255,7 @@ class KubernetesExecutor(Executor):
                     },
                     {
                         "name": "AWS_S3_ENDPOINT_URL",
-                        "value": settings.AWS_S3_ENDPOINT_URL,
+                        "value": self._s3_endpoint(),
                     },
                 ]
             )
@@ -202,32 +263,34 @@ class KubernetesExecutor(Executor):
         jobspec = {
             "apiVersion": "batch/v1",
             "kind": "Job",
-            "metadata": {"name": {self.container_name}},
+            "metadata": {"name": f"{self.job_name}"},
             "spec": {
                 "template": {
                     "spec": {
                         "containers": [
                             {
-                                "name": f"{self.container_name}-runner",
-                                "image": self._exec_image_repo_tag,
+                                "name": f"{self.job_name}-runner",
+                                "image": self._exec_image_repo_tag.replace(
+                                    "registry:5000", f"{self.get_registry_ip()}:5000"
+                                ),
                                 "args": ["serve"],
                                 "env": environment,
                             }
                         ],
                         "restartPolicy": "Never",
-                    },
-                    "backoffLimit": 4,
-                }
+                    }
+                },
+                "backoffLimit": 4,
             },
         }
 
+        logger.debug(f"Jobspec is:\n {jobspec}")
         try:
-            self.create_job(jobspec)
+            self.create_job(json.dumps(jobspec))
             self._await_container_ready()
             try:
-
                 response = requests.post(
-                    f"http://{container_ip}:8080/invocations",
+                    f"http://{self.container_ip}:8080/invocations",
                     json=self._get_invocation_json(
                         input_civs=input_civs, input_prefixes=input_prefixes
                     ),
@@ -237,10 +300,11 @@ class KubernetesExecutor(Executor):
                 raise ComponentException("Time limit exceeded")
         finally:
             self._set_task_logs()
-            delete_job(name=self.container_name)
+            self.delete_job(name=self.job_name)
 
-        response = response.json()
-        exit_code = int(response["return_code"])
+        logger.debug(f"Response from {response.text}")
+        r = response.json()
+        exit_code = int(r["return_code"])
 
         if exit_code > 128:
             raise ComponentException(
@@ -250,15 +314,17 @@ class KubernetesExecutor(Executor):
             raise ComponentException(user_error(self.stderr))
 
     def _await_container_ready(self):
+        logger.debug("Await container ready")
         attempts = 0
         while True:
             attempts += 1
+            logger.debug(f"Await container ready, attempt {attempts}")
 
             try:
                 # Timeout is from the SageMaker API definitions
                 # https://docs.aws.amazon.com/sagemaker/latest/dg/your-algorithms-batch-code.html#your-algorithms-batch-algo-ping-requests
                 response = requests.get(
-                    f"http://{container_ip}:8080/ping",
+                    f"http://{self.container_ip}:8080/ping",
                     timeout=2,
                 )
             except requests.exceptions.RequestException:
@@ -271,9 +337,11 @@ class KubernetesExecutor(Executor):
 
     def _set_task_logs(self):
         try:
-            loglines = container_logs
+            loglines = self.container_logs
         except ObjectDoesNotExist:
             return
 
+        print(loglines)
         self._stderr = []
-        self._stdout = loglines
+        # Expects a list of lines, so we should give it that
+        self._stdout = loglines.split("\n")
